d0f58a77b3e5bff65cdab4c10c77121917426f4d
orpax <pramanaaatio11@gmail.com>
2025-07-19 23:35:08 +0800

    Pembelajaran Pertama Tentang Node JS

---
+++ env.js
@@ -0,0 +1,2 @@
+console.log("Nama Saya : " + process.env.NAME);
+console.log("Profesi Saya: " + process.env.PROFESSION);
---
+++ index.js
@@ -0,0 +1,25 @@
+// console.log("Hello World");
+
+// const fs = require("fs");
+
+// fs.readFile("node_js.txt", "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end("Hello World\n");
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server running at http://localhost:3000/");
+// });
+
+console.log("Hello World");
+
+process.exit(0);
+
+// process.exitCode = 1;
