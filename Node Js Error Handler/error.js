fa78b1fd63aea7745a454d11987699cbe8092f8a
Tio Pramanaa <{your_email}>
2025-07-24 23:13:34 +0800

    Melanjutkan memahami Error Promise

--- Node Js Error Handler/error.js
+++ Node Js Error Handler/error.js
@@ -7,5 +7,58 @@
 
 // throw new Error("Iam the Error!");
 
-const { CustomError } = require("./CustomError");
-throw new CustomError("This Is Da Error");
+// const { CustomError } = require("./CustomError");
+// throw new CustomError("This Is Da Error");
+
+// handel exception using try and catch
+
+// try {
+//   doSomething();
+// } catch (e) {
+//   console.log("Error Occured!");
+//   console.log(e);
+// }
+
+// const { reject } = require("lodash");
+const fetch = require("node-fetch");
+
+// // function doSomething() {
+//   const data = fetch("http://localhost:3000/api");
+//   console.log("Ini Adalah Error function!");
+// }
+
+// unchaught exception
+// process.on("uncaughtException", (err) => {
+//   console.log("Ini adalah Error Yang Uncaught");
+//   process.exit(1);
+// });
+
+// Exceptions with promise
+
+// const promise = new Promise((resolve, reject) => {
+//   if (false) {
+//     resolve(doSomething());
+//   } else {
+//     reject(doSomething());
+//   }
+// });
+
+// promise
+//   .then((val) => {
+//     console.log(val);
+//   })
+//   .catch((err) => {
+//     console.log("Terjadi Error");
+// //     console.log(err);
+//   });
+
+const promise = new Promise((resolve, reject) => {
+  fetch("http://localhost:5173/")
+    .then((res) => res.json())
+    .then((data) => resolve(data))
+    .catch((err) => reject(err));
+});
+
+promise
+  .then((data) => console.log("Hasil : ", JSON.stringify(data, null, 2)))
+  .catch((err) => console.log("Error : ", err.message || err));
