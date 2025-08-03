eadc7ff6effbbf3ada3108a00456ea4476c7bd67
orpax <pramanaaatio11@gmail.com>
2025-08-03 22:27:42 +0800

    Asynchro and Synchro

--- async/async.js
+++ async/async.js
@@ -1,12 +1,29 @@
+// Synchronous programming adalah eksekusi kode yang berjalan satu per satu secara berurutan
+
+// console.log("Starting file operations...");
+
+// function sleep(miliseconds) {
+//   let startTime = new Date().getTime();
+//   console.log("Sleeping for " + miliseconds + " milliseconds...");
+//   while (new Date().getTime() < startTime + miliseconds) {
+//     console.log("Still sleeping...");
+//   }
+//   console.log("Woke up after " + miliseconds + " milliseconds!");
+// }
+
+// sleep(2000);
+
+// console.log("File operations will continue after sleep...");
+
+// Asynchronous programming memungkinkan kode untuk berjalan tanpa harus menunggu proses sebelumnya selesai dengan menggunakan timeout
+
 console.log("Starting file operations...");
 
 function sleep(miliseconds) {
-  let startTime = new Date().getTime();
-  console.log("Sleeping for " + miliseconds + " milliseconds...");
-  while (new Date().getTime() < startTime + miliseconds) {
-    console.log("Still sleeping...");
-  }
-  console.log("Woke up after " + miliseconds + " milliseconds!");
+  console.log("Operataion Started");
+  setTimeout(() => {
+    console.log("Operation Is Running");
+  }, miliseconds);
 }
 
 sleep(2000);
