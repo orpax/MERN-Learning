b759e815eb6855609051b82b5b5133e19129c418
Tio Pramanaa <{your_email}>
2025-08-12 10:09:50 +0800

    Fix Get Request

--- node_js_crud/methods/get-request.js
+++ node_js_crud/methods/get-request.js
@@ -1,1 +1,18 @@
-modelu.export = (req, res) => {};
+module.exports = (req, res) => {
+  if (req.url === "/api/movies") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(req.movies));
+    res.end();
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        title: "Not Found",
+        message: "The requested resource was not found",
+        status: "Failed",
+        timestamp: new Date().toISOString(),
+      })
+    );
+  }
+};
